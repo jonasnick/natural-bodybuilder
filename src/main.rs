52a9fdeb686273379e73bd3542ae40830b2588dc
Jonas Nick <jonasd.nick@gmail.com>
2019-06-10 20:13:50 +0000

    remove randomize

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
-use rand::prelude::*;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Ingredient {
@@ -155,41 +154,6 @@ fn optimize(target: &NormalizedTarget, ingredients: &Ingredients, steps: usize) 
         //println!("Add {}, cost {}", best_ingredient.unwrap(), target.evaluate(&proposal, ingredients));
     }
     proposal
-}
-
-fn randomize(target: &NormalizedTarget, ingredients: &Ingredients, steps: usize) -> Proposal {
-    let maxn = 100;
-
-    let mut best_proposal = None;
-    let mut min_cost = None;
-    for _ in 0..steps {
-        let mut proposal = Proposal(HashMap::new());
-        for (name, _) in &ingredients.0 {
-            proposal.0.insert(name.to_string(), 0);
-        }
-        for (name, _) in &ingredients.0 {
-            let n: u64 = random();
-            proposal.0.insert(name.to_string(), n % maxn);
-        }
-        let cost = target.evaluate(&proposal, ingredients);
-        println!("Add {:?}, cost {}", &proposal, cost);
-        min_cost = match min_cost {
-            None => {
-                best_proposal = Some(proposal);
-                Some(cost)
-            }
-            Some(min_cost) => {
-                if cost < min_cost {
-                    best_proposal = Some(proposal);
-                    Some(cost)
-                } else {
-                    Some(min_cost)
-                }
-            }
-        };
-    }
-    println!("Best: Add {:?}, cost {}", best_proposal.clone().unwrap(), min_cost.unwrap());
-    best_proposal.unwrap()
 }
 
 fn help() {
